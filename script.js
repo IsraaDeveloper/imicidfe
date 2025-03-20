81a6f7b55cdc1e8ac16c5e9fe313ab9bb5683908
IsraaDeveloper <israadeveloper@gmail.com>
2025-03-20 22:27:00 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+async function shortenLink() {
+    const longUrl = document.getElementById("longUrl").value;
+    const customSlug = document.getElementById("customSlug").value;
+
+    if (!longUrl) {
+        alert("Masukkan URL terlebih dahulu!");
+        return;
+    }
+
+    const response = await fetch("http://localhost:3000/shorten", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ long_url: longUrl, custom_slug: customSlug }),
+    });
+
+    const data = await response.json();
+    if (data.error) {
+        alert(data.error);
+    } else {
+        document.getElementById("result").innerHTML = `
+            <p>Short Link: <a href="${data.short_url}" target="_blank">${data.short_url}</a></p>
+        `;
+    }
+}
